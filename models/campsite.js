9a43b76a91189c907f9263b0c4040ba1be8791bb
Krosis855 <jtspalding22@gmail.com>
2021-09-02 00:28:58 -0700

    Task 1 Complete

--- models/campsite.js
+++ models/campsite.js
@@ -1,22 +1,19 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const campsiteSchema = new Schema(
-  {
+const campsiteSchema = new Schema({
     name: {
-      type: String,
-      required: true,
-      unique: true,
+        type: String,
+        required: true,
+        unique: true
     },
     description: {
-      type: String,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
 
 const Campsite = mongoose.model('Campsite', campsiteSchema);
 
